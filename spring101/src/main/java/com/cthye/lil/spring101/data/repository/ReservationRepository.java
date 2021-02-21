bb6fa5dfa5f9aa0fd1e254fa207803664f20fab8
cman8564134 <clement_18@live.com.my>
2021-02-21 19:43:42 +0800

    implemented method using Spring Data Query.

--- spring101/src/main/java/com/cthye/lil/spring101/data/repository/ReservationRepository.java
+++ spring101/src/main/java/com/cthye/lil/spring101/data/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package com.cthye.lil.spring101.data.repository;
 
 import com.cthye.lil.spring101.data.entity.Reservation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,8 @@ import java.sql.Date;
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
     Iterable<Reservation> findReservationByDate(Date date);
+
+    //build method using Spring Data Query
+    @Query("SELECT r FROM Reservation r WHERE r.date = ?1 ORDER BY r.id")
+    Iterable<Reservation> lookupReservationByDate(Date date);
 }
