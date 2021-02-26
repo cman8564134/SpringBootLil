5ed4649a4511bc4382de8103d2d27b9f2f5c7960
cman8564134 <clement_18@live.com.my>
2021-02-26 22:55:31 +0800

    Added function to enable /search resources at Room Repository.

--- spring101/src/main/java/com/cthye/lil/spring101/data/repository/RoomRepository.java
+++ spring101/src/main/java/com/cthye/lil/spring101/data/repository/RoomRepository.java
@@ -2,9 +2,11 @@ package com.cthye.lil.spring101.data.repository;
 
 import com.cthye.lil.spring101.data.entity.Room;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
+    Iterable<Room> findByRoomId(@Param(("RoomID")) Long RoomID);
 }
 
